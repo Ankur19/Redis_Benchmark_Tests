a771a3b3d26eed91691009f756710d370347eb3d
ankur19 <ankursaikia08@gmail.com>
2020-11-27 03:13:38 -0600

    Added new threaded results

--- run_redis_benchmark.sh
+++ run_redis_benchmark.sh
@@ -13,10 +13,11 @@ echo "Saving results in " $dir;
 for connections in  512; do
 	for payload_size in  3 100 1000; do
 	for pipeline_size in  1 5 10; do
-	echo "running for pipeline size" $pipeline_size ", payload size of" $payload_size ", connections" + $connections;
+	for num_threads in 12 24 36 48; do
+	echo "running for pipeline size" $pipeline_size ", payload size of" $payload_size ", connections" $connections ", with $num_threads threads";
 		date;
-		touch $dir/"iothreads_"$iothreads"_pipeline_"$pipeline_size"_connections_"$connections"_payload_"$payload_size".txt";
-		redis-benchmark -t mset,set,get,hset,lrange_100,xadd -c $connections -P $pipeline_size -n $n --threads 48 -d $payload_size >> $dir/"iothreads_"$iothreads"_pipeline_"$pipeline_size"_connections_"$connections"_payload_"$payload_size".txt";
+		redis-benchmark -t mset,set,get,hset,lrange_100,xadd -c $connections -P $pipeline_size -n $n --threads $num_threads -d $payload_size >> $dir/"iothreads_"$iothreads"_pipeline_"$pipeline_size"_connections_"$connections"_payload_"$payload_size"_threads_"$num_threads".txt";
+	done;
 	done;
 	done;
 done;
